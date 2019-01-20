14e9f3052a9cb691833f74dbf67f22ac7911121c
Philipp Staender <philipp.staender@gmail.com>
2019-01-20 11:21:23 +0100

    display if no records are recorded

--- lib/punchcard.rb
+++ lib/punchcard.rb
@@ -12,7 +12,7 @@ class PunchCard
   HOURLY_RATE_PATTERN = /^\s*(\d+)([^\d]+)*\s*/i
   TIME_POINT_PATTERN  = /^((\d+|.+?\s[\+\-]\d{4}?\s*)(\-)*(\d+|\s.+\d?)*)$/
   META_KEY_PATTERN    = /^([a-zA-Z0-9]+)\:\s*(.*)$/
-  VERSION             = '1.0.3'
+  VERSION             = '1.0.4'
 
   attr_accessor :project
 
